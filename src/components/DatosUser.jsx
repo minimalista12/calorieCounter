757c98bd0e418fecb76b007374ee97ed97d5c345
NatyNatur <naticcg@gmail.com>
2018-03-23 00:33:31 -0300

    page_3

--- src/components/DatosUser.jsx
+++ src/components/DatosUser.jsx
@@ -3,16 +3,22 @@ import PropTypes from 'prop-types'
 import { viewForm } from '../actions'
 
 const DatosUser = ({ user, dispatch }) => (
-  <div className="container-fluid">
-    <h2>Ingesta diaria de calorías</h2>
-    <div>
-    <label > { user !==undefined && user.calorias!==undefined ? user.calorias : null}</label>
+  <div>
+    <div className="btnBack">
+      <button onClick={() => dispatch(viewForm(true))}>Atrás</button>
     </div>
-    <h3>recomendadas para ti</h3>
-    <button onClick={()=>dispatch(viewForm(true))}>Atras</button>  
-    {/* <button onClick={()=>dispatch(viewForm(true))}>Siguiente</button>  */} 
+    <div className="thirdPage">
+      <h2>Ingesta diaria de calorías</h2>
+      <div className="calculatedCalories">
+        <label > {user !== undefined && user.calorias !== undefined ? user.calorias : null}</label>
+      </div>
+      <h3>recomendadas para ti</h3>
+    </div>
+    <div className="btnNext">
+      <button /*onClick={() => dispatch(viewForm(true))}*/>Agregar alimentos</button>
+    </div>
   </div>
-  
+
 )
 
 export default DatosUser;
